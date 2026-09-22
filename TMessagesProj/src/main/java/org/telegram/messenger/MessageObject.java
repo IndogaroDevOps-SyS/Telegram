5b40c928324f38e14521a84d4b8e09584131f217
IndogaroDevOps-SyS <cloudfilekerjaan@gmail.com>
2026-09-22 21:16:23 +0700

    media save

--- TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
@@ -8655,11 +8655,7 @@ public class MessageObject {
             return;
         }
         boolean hasUrls = applyEntities();
-        boolean noforwards = messageOwner != null && messageOwner.noforwards;
-        if (!noforwards) {
-            final long dialogId = getDialogId();
-            noforwards = MessagesController.getInstance(currentAccount).isPeerNoForwards(dialogId);
-        }
+        boolean noforwards = false;
 
         textLayoutBlocks = new ArrayList<>();
         textWidth = 0;
