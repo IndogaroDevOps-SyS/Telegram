247992eb3516325c6adc411c4afe1f2f0c2253c9
IndogaroDevOps-SyS <cloudfilekerjaan@gmail.com>
2026-09-24 14:59:19 +0700

    tes

--- TMessagesProj/src/main/java/id/indogaro/messenger/DownloadController.java
+++ TMessagesProj/src/main/java/id/indogaro/messenger/DownloadController.java
@@ -625,6 +625,7 @@ public class DownloadController extends BaseController implements NotificationCe
     }
 
     public boolean canDownloadMedia(int type, long size) {
+        if (type == 1 || type == AUTODOWNLOAD_TYPE_VIDEO) { return false; }
         if (type == AUTODOWNLOAD_TYPE_VIDEO) { return false; }
         Preset preset;
         int networkType = ApplicationLoader.getAutodownloadNetworkType();
